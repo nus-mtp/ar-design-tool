d37677a41d5c1944554121c54981ad9d004da29c
a0099654 <a0099654@u.nus.edu>
2015-09-12 10:57:42 +0800

    Adding virtual button

---
+++ PrototypeVer1/Assets/Userinterfacebutton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Userinterfacebutton : MonoBehaviour {
+	bool buttonClicked = false;
+	private GameObject item;
+
+	// Use this for initialization
+	void Awake () {
+		item = GameObject.FindGameObjectWithTag ("Zinc");
+		item.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (buttonClicked) {
+			ShowItem();
+		}
+	}
+
+	public void IsShow(){
+		buttonClicked = true;
+	}
+
+	public void ShowItem(){
+		item.SetActive (true);
+	}
+	
+}
