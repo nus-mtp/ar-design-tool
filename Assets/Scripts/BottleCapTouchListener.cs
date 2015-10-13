2bc359e6ac9f627ff1df1eea5f9720b0adf08c78
a0099654 <a0099654@u.nus.edu>
2015-10-13 18:17:16 +0800

    [ADDED]Add arrow up and down

--- Assets/Scripts/BottleCapTouchListener.cs
+++ Assets/Scripts/BottleCapTouchListener.cs
@@ -9,16 +9,23 @@ public class BottleCapTouchListener : AbstractTouchListener {
 
 	bool statusTracked;
 	public static bool isClicked = false;
+	private GameObject arrow;
 
 	float switchOnTime = 1.5f;
 	float switchOffTime = 1.5f;
 	float switchOnCounter;
 	float switchOffCounter;
 
+	float jumpUpTime = 1.5f;
+	float jumpDownTime = 1.5f;
+	float jumpUpTimeCounter;
+	float jumpDownTimeCounter;
+
 	void Start(){
 		halo = GetComponent ("Halo");
 		switchOnCounter = 0f;
 		switchOffCounter = 0f;
+		arrow = GameObject.Find ("Arrow");
 	}
 
 	void Update(){
@@ -27,11 +34,13 @@ public class BottleCapTouchListener : AbstractTouchListener {
 
 		if (statusTracked) {
 			flickering ();
+			jumpUpAndDown();
 			resetTimeCounter();
 		}
 
 		if (isClicked == true) {
 			setHalo (false);
+			arrow.SetActive(false);
 		}
 
 	}
@@ -52,6 +61,19 @@ public class BottleCapTouchListener : AbstractTouchListener {
 		}
 	}
 
+	//to jump up and down of an object
+	public void jumpUpAndDown(){
+		if (jumpUpTimeCounter < jumpUpTime) {
+			jumpUpTimeCounter += Time.deltaTime;
+			arrow.transform.Translate (Vector3.up * 10 * Time.deltaTime, Space.World);
+		} else {
+			if (jumpDownTimeCounter < jumpDownTime) {
+				jumpDownTimeCounter += Time.deltaTime;
+				arrow.transform.Translate (Vector3.down * 10 * Time.deltaTime, Space.World);
+			}
+		}
+	}
+
 	public override void touchHandler()
 	{	pill= this.gameObject.transform.GetChild(0);
 		pill.gameObject.SetActive (true);
@@ -66,5 +88,11 @@ public class BottleCapTouchListener : AbstractTouchListener {
 			switchOnCounter = 0f;
 			switchOffCounter = 0f;
 		}
+		if (jumpDownTimeCounter > 1.5f && jumpUpTimeCounter > 1.5f) {
+			jumpDownTimeCounter = 0f;
+			jumpUpTimeCounter = 0f;
+		}
 	}
+
+
 }
