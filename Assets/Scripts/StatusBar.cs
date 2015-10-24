afed625516d03925cc1c1f7e4116f8125c1c76d1
Jeffrey Effendy <wawa_union.zone@live.com>
2015-10-24 11:25:06 +0800

    merged

---
+++ Assets/Scripts/StatusBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatusBar : MonoBehaviour {
+    public Text statusBar;
+    private UndoStack undostack;
+	// Use this for initialization
+	void Start () {
+        undostack = UndoStack.getInstance();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
