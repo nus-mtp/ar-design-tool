d54210aebb59e2d51388ddcb755fb6b4a47c5af7
Jeffrey Effendy <wawa_union.zone@live.com>
2016-01-02 12:10:48 +0800

    babylon update

--- Phase 02/Frontend/web/babylon_demo/babylon_init.js
+++ Phase 02/Frontend/web/babylon_demo/babylon_init.js
@@ -1,20 +1,31 @@
 var canvas = document.getElementById("renderCanvas");
+var option = document.getElementById("model-option");
 var engine = new BABYLON.Engine(canvas, true);
+var change = false;
+var model = "teddy.obj";
 var createScene = function() {
     var scene = new BABYLON.Scene(engine);
-    var cam = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, 0, 5, new BABYLON.Vector3(0, 3, 0), scene);
-    var loader = new BABYLON.AssetsManager(scene);
+    var cam = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, 0, 50, new BABYLON.Vector3(0, 3, 0), scene);
+    cam.attachControl(canvas);
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0,1,0),scene);
-    var demo = loader.addMeshTask("demo02", "", "assets/", "demo02.obj");
-    demo.onSuccess = function() {
-        console.log("loaded");
+    light.intensity = 0.5;
+    BABYLON.SceneLoader.ImportMesh("", "assets/", model, scene, function (meshes) {});
+    scene.beforeRender = function() {
+        if(change) {
+            scene.meshes.forEach(function(mesh) {
+                mesh.dispose();
+            });            
+            BABYLON.SceneLoader.ImportMesh("", "assets/", model, scene, function (meshes) {});
+            change = false;
+        }
     }
-    loader.onFinish = function() {
-        engine.runRenderLoop(function() {
-            scene.render();
-        })
-    };
-    loader.load();
     return scene;
 }
-createScene();
+var scene = createScene();
+engine.runRenderLoop(function() {
+    scene.render();
+});
+option.addEventListener("change", function(evt) {
+    model = evt.target.value; 
+    change = true;
+});
