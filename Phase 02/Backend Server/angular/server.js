ec3799f944e6782ab9910807ef58c0fd59fb460a
John Lim <johnlimed@gmail.com>
2016-01-28 13:08:55 +0800

    successfully load partial pages using angular-ui-router

--- Phase 02/Backend Server/angular/server.js
+++ Phase 02/Backend Server/angular/server.js
@@ -9,12 +9,13 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'dot');
 // app.enable('view cache');
 
+app.use('/partials', express.static(path.join(__dirname, 'views/partials')));
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use('/views', express.static(path.join(__dirname, 'views')));
-app.use('/partials', express.static(path.join(__dirname, 'views/partials')));
 
 app.get('/', function (req, res) {
-	res.render('home');
+	res.render('index');
 });
 
 app.listen(3000, function() {
