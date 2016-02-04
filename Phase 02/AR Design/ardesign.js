a272b3d8cee59d610c97c347eec0508a966cd2a8
John Lim <johnlimed@gmail.com>
2016-02-04 13:09:23 +0800

    added express routing and changed the folder structure

--- Phase 02/AR Design/ardesign.js
+++ Phase 02/AR Design/ardesign.js
@@ -5,17 +5,20 @@ var express	= require('express'),
 var app = express();
 
 app.engine('dot', engine.__express);
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, 'static/views'));
 app.set('view engine', 'dot');
 // app.enable('view cache');
 
-app.use('/partials', express.static(path.join(__dirname, 'views/partials')));
-app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 app.use('/static', express.static(path.join(__dirname, 'static')));
-app.use('/views', express.static(path.join(__dirname, 'views')));
+app.use('/views', express.static(path.join(__dirname, 'static/views')));
+app.use('/partials', express.static(path.join(__dirname, 'static/views/partials')));
 
 app.get('/', function (req, res) {
 	res.render('index');
+});
+
+app.get('/login', function (req, es) {
+	res.render('login');
 });
 
 app.listen(3000, function() {
