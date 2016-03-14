3ec8dc45d06c96e3553c4dd3a277a998ec6f5775
John Lim <johnlimed@gmail.com>
2016-03-14 22:47:11 +0800

    S8.1 fixed jslint errors

--- app/server/modules/unity.js
+++ app/server/modules/unity.js
@@ -2,21 +2,20 @@ var unity_var 	= require('../config/unity');
 var fs 			= require('fs');
 
 module.exports = function(uid, pid) {
-	var project_path = unity_var.project_path + uid + '/' + pid + '/';
-	var unity_cmd = '"' + unity_var.unity + '" -createProject "' + project_path + '" -importPackage "' + unity_var.app_builder + '"';
-		
+	var project_path 	= unity_var.project_path + uid + '/' + pid + '/';
+	var unity_cmd 		= '"' + unity_var.unity + '" -createProject "' + project_path + '" -importPackage "' + unity_var.app_builder + '"';
+	var makePath 		= unity_var.project_path + uid + '/';
 	try {
-		console.log(fs.statSync(project_path))	
+		console.log(fs.statSync(project_path));	
 	} catch (e) {
 		try {
-			var makePath = unity_var.project_path + uid + '/';
-			fs.mkdirSync(makePath) 
+			fs.mkdirSync(makePath); 
 			makePath = makePath + pid + '/';
 			fs.mkdirSync(makePath);
 		} catch (e) {
 			if (e.code == 'EEXIST') {
 				try {
-					var makePath = unity_var.project_path + uid + '/' + pid + '/';
+					makePath = unity_var.project_path + uid + '/' + pid + '/';
 					fs.mkdirSync(makePath);
 				} catch (e) {
 					console.log(e);
@@ -24,7 +23,7 @@ module.exports = function(uid, pid) {
 			}
 		}
 	}
-	console.log(unity_cmd)
+	console.log(unity_cmd);
 	const exec	= require('child_process').exec;
 	const unity	= exec(unity_cmd, function(error, stdout, stderr) {
 			console.log("stdout: " + stdout);
