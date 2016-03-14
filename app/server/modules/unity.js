731075203a80c423bd39a7580064355db9c8eefe
John Lim <johnlimed@gmail.com>
2016-03-14 21:34:16 +0800

    changed syntax of calling cmd

--- app/server/modules/unity.js
+++ app/server/modules/unity.js
@@ -3,7 +3,7 @@ var fs 			= require('fs');
 
 module.exports = function(uid, pid) {
 	var project_path = unity_var.project_path + uid + '/' + pid + '/';
-	var unity_cmd = '"' + unity_var.unity + '" -createProject "' + project_path + '" -importPackage "' + unity_var.app_builder + '" -quit';
+	var unity_cmd = '"' + unity_var.unity + '" -createProject "' + project_path + '" -importPackage "' + unity_var.app_builder;
 		
 	try {
 		console.log(fs.statSync(project_path))	
@@ -25,17 +25,16 @@ module.exports = function(uid, pid) {
 		}
 	}
 
-	const spawn = require('child_process').exec;
-	const unity	= spawn(unity_cmd);
-
-	unity.stdout.on('data', (data) => {
-		console.log(`stdout: ${data}`);
+	const exec	= require('child_process').exec;
+	const unity	= exec(unity_cmd, {
+		(error, stdout, stderr) => {
+			console.log(`stdout: ${stdout}`);
+			console.log(`stderr: ${stderr}`);		
+			if (error !== null) {
+				console.log(`exec error: ${error}`);
+			}
+		}
 	});
-
-	unity.stderr.on('data', (data) => {
-		console.log(`stderr: ${data}`);
-	});
-
 	unity.on('close', (code) => {
 		console.log(`child process exited with code ${code}`);
 	});
