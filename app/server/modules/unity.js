5cf94eb5b03629c70b41429db15393001e516984
John Lim <johnlimed@gmail.com>
2016-03-14 21:55:41 +0800

    made changes to coverage

--- app/server/modules/unity.js
+++ app/server/modules/unity.js
@@ -38,30 +38,4 @@ module.exports = function(uid, pid) {
 	unity.on('close', (code) => {
 		console.log(`child process exited with code ${code}`);
 	});
-	// const ping 		= spawn('tracert www.google.com');
-	// const git		= spawn('git branch', ['-l']);
-
-	// git.stdout.on('data', (data) => {
-	// 	console.log(`stdout: ${data}`);
-	// });
-
-	// git.stderr.on('data', (data) => {
-	// 	console.log(`stderr: ${data}`);
-	// });
-
-	// git.on('close', (code) => {
-	// 	console.log(`child process exited with code ${code}`);
-	// });		
-
-	// ping.stdout.on('data', (data) => {
-	// 	console.log(`stdout: ${data}`);
-	// });
-
-	// ping.stderr.on('data', (data) => {
-	// 	console.log(`stderr: ${data}`);
-	// });
-
-	// ping.on('close', (code) => {
-	// 	console.log(`child process exited with code ${code}`);
-	// });		
 };
