87c7df83795bd05191293d24fe976502097dc61d
John Lim <johnlimed@gmail.com>
2016-03-14 21:12:03 +0800

    S8.1 now calls the unity build project cmd

--- app/server/modules/unity.js
+++ app/server/modules/unity.js
@@ -1,33 +1,71 @@
 var unity_var = require('../config/unity');
+var fs = require('fs');
 
-module.exports = function() {
+module.exports = function(uid, pid) {
+	var project_path = unity_var.project_path + uid + '/' + pid + '/';
+	// var unity_cmd = unity_var.unity;
+	var unity_cmd = '"' + unity_var.unity + '" -createProject "' + project_path + '" -importPackage "' + unity_var.app_builder + '" -quit';
+		
+	try {
+		console.log(fs.statSync(project_path))	
+	} catch (e) {
+		try {
+			var makePath = unity_var.project_path + uid + '/';
+			fs.mkdirSync(makePath) 
+			makePath = makePath + pid + '/';
+			fs.mkdirSync(makePath);
+		} catch (e) {
+			if (e.code == 'EEXIST') {
+				try {
+					var makePath = unity_var.project_path + uid + '/' + pid + '/';
+					fs.mkdirSync(makePath);
+				} catch (e) {
+					console.log(e);
+				}
+			}
+		}
+	}
+
 	const spawn 	= require('child_process').exec;
 
-	// const unity		= spawn(unity_var.unity)
-	const ping 		= spawn('tracert www.google.com');
-	const git		= spawn('git branch', ['-l']);
-
-	git.stdout.on('data', (data) => {
+	const unity		= spawn(unity_cmd);
+	// console.log(project_path)
+	// console.log(unity_cmd)
+	unity.stdout.on('dara', (data) => {
 		console.log(`stdout: ${data}`);
 	});
 
-	git.stderr.on('data', (data) => {
+	unity.stderr.on('dara', (data) => {
 		console.log(`stderr: ${data}`);
 	});
 
-	git.on('close', (code) => {
+	unity.on('close', (code) => {
 		console.log(`child process exited with code ${code}`);
-	});		
-
-	ping.stdout.on('data', (data) => {
-		console.log(`stdout: ${data}`);
 	});
+	// const ping 		= spawn('tracert www.google.com');
+	// const git		= spawn('git branch', ['-l']);
 
-	ping.stderr.on('data', (data) => {
-		console.log(`stderr: ${data}`);
-	});
+	// git.stdout.on('data', (data) => {
+	// 	console.log(`stdout: ${data}`);
+	// });
 
-	ping.on('close', (code) => {
-		console.log(`child process exited with code ${code}`);
-	});		
+	// git.stderr.on('data', (data) => {
+	// 	console.log(`stderr: ${data}`);
+	// });
+
+	// git.on('close', (code) => {
+	// 	console.log(`child process exited with code ${code}`);
+	// });		
+
+	// ping.stdout.on('data', (data) => {
+	// 	console.log(`stdout: ${data}`);
+	// });
+
+	// ping.stderr.on('data', (data) => {
+	// 	console.log(`stderr: ${data}`);
+	// });
+
+	// ping.on('close', (code) => {
+	// 	console.log(`child process exited with code ${code}`);
+	// });		
 };
