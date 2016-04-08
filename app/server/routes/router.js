806b95b6b73edffcbb61e635104ecdd20d00d074
John Lim <johnlimed@gmail.com>
2016-04-09 01:51:32 +0800

    removed code that was causing errors

--- app/server/routes/router.js
+++ app/server/routes/router.js
@@ -33,21 +33,4 @@ router.get('/logout', function(req, res) {
 	res.redirect('/');
 });
 
-router.use(function(req, res, next){
-  res.status(404);
-  // // respond with html page
-  // if (req.accepts('html')) {
-  //   res.render('404', { url: req.url });
-  //   return;
-  // }
-
-  // // respond with json
-  // if (req.accepts('json')) {
-  //   res.send({ error: 'Not found' });
-  //   return;
-  // }
-  // default to plain-text. send()
-  res.redirect('/');
-});
-
 module.exports = router;
